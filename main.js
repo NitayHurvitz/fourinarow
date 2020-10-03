56bcd61b907fcbda7c824c75412c01550137bc19
Nitay Hurvitz <nitay.hurvitz@verint.com>
2020-10-03 18:52:41 +0300

    cr comments

--- main.js
+++ main.js
@@ -1,120 +1,145 @@
 
 const board = document.getElementById("board")
-let turn = "red"
+const rowsInput = document.getElementById("input-rows")
+const colsInput = document.getElementById("input-cols")
 
-function createCoinElement(diameterInPx) {
+let turn;
+let gameStarted = false;
+
+const PLAYER_1 = {
+    class: "player-1",
+    color: "red"
+}
+const PLAYER_2 = {
+    class: "player-2",
+    color: "blue"
+}
+const CSS_VARS = {
+    BORDER_WIDTH: "--border-width",
+    CELL_SIZE: "--cell-size",
+    PLAYER_1_COLOR: "--player-1-color",
+    PLAYER_2_COLOR: "--player-2-color",
+}
+
+const COIN_CLASS = "coin"
+const CELL_CLASS = "cell"
+const COLUMN_CLASS = "column"
+const BOARD_WIDTH_IN_PX = 400
+
+function createCoinElement() {
     const coin = document.createElement("div")
-    coin.className = "coin"
-    coin.style.height = diameterInPx
-    coin.style.width = diameterInPx
-    coin.style.background = turn
-    coin.style.borderRadius = diameterInPx
+    coin.classList.add(COIN_CLASS)
+    coin.classList.add(turn.class)
+
     return coin
 }
 
-function createCellElement(row, col, cellSize) {
+function createCellElement(row, col) {
     const cell = document.createElement("div")
-    cell.className = "cell"
+    cell.className = CELL_CLASS
     cell.row = row
     cell.col = col
-    cell.style.height = `${cellSize - 6}px`
-    cell.style.width = `${cellSize - 6}px`
-    cell.style.background = "white"
 
     return cell;
 }
 
-function createColumnElement(colNum, numOfRows, cellSize) {
+function createColumnElement(colNum, numOfRows) {
     const column = document.createElement("div")
-    column.className = "column"
+    column.className = COLUMN_CLASS
     column.number = colNum
 
     for (let rowNum = 1; rowNum <= numOfRows; rowNum++) {
-        column.appendChild(createCellElement(rowNum, colNum, cellSize))
+        column.appendChild(createCellElement(rowNum, colNum))
     }
-    return column;
 
+    return column;
 }
 
 function startBoard() {
-    const numOfRows = document.getElementById("input-rows").value
-    const numOfColumns = document.getElementById("input-cols").value
-
+    gameStarted = false;
+    turn = PLAYER_1
     board.innerHTML = ""
-    boardWidthPx = 400;
-    boardHeighPx = boardWidthPx * numOfRows / numOfColumns;
 
-    cellSize = boardWidthPx / numOfColumns
+    const numOfRows = rowsInput.value
+    const numOfColumns = colsInput.value
 
-    board.style.height = `${boardHeighPx}px`
-    board.style.width = `${boardWidthPx}px`
+    board.style.setProperty(CSS_VARS.BORDER_WIDTH, BOARD_WIDTH_IN_PX)
+
+    cellSizeInPx = BOARD_WIDTH_IN_PX / numOfColumns
+
+    board.style.setProperty(CSS_VARS.CELL_SIZE, `${cellSizeInPx}px`)
+    board.style.setProperty(CSS_VARS.PLAYER_1_COLOR, PLAYER_1.color)
+    board.style.setProperty(CSS_VARS.PLAYER_2_COLOR, PLAYER_2.color)
+
     for (let colNum = 1; colNum <= numOfColumns; colNum++) {
-        board.appendChild(createColumnElement(colNum, numOfRows, cellSize))
+        board.appendChild(createColumnElement(colNum, numOfRows))
     }
 }
 
-function handleBoardClicked(event) {
-    const targetCol = getColumnFromEventTarget(event.target)
+function onBoardClicked(event) {
+    const targetCol = getParentColumnOfNode(event.target)
     if (targetCol === undefined)
         return
 
-    cells = Array.from(targetCol.getElementsByClassName("cell"))
-    const lastCell = getLastFreeCell(cells);
+    const lastCell = getFreeCellOfColumn(targetCol);
 
     if (lastCell !== undefined) {
-        lastCell.appendChild(createCoinElement(lastCell.style.height))
-        turn = turn === "red" ? "blue" : "red"
-        targetCol.style.background = turn
+        gameStarted = true;
+        lastCell.appendChild(createCoinElement())
+        turn = turn === PLAYER_1 ? PLAYER_2 : PLAYER_1
+        targetCol.classList.add(turn.class)
     }
 
 }
 
-function getColumnFromEventTarget(target) {
-    const targetCol = target.className == "column" ?
-        target :
-        target.className == "cell" ?
-            target.parentElement :
-            target.className == "coin" ?
-                target.parentElement.parentElement :
-                undefined
-
-    return targetCol;
+function getParentColumnOfNode(node) {
+    if (node.classList.contains(COLUMN_CLASS))
+        return node
+    if (node.classList.contains(CELL_CLASS))
+        return node.parentElement
+    if (node.classList.contains(COIN_CLASS))
+        return node.parentElement.parentElement
+    return undefined
 }
 
-function getLastFreeCell(cells) {
-    let lastCell = undefined;
-    cells.forEach(cell => {
-        if (!cell.hasChildNodes()) {
-            lastCell = cell;
-        }
-    })
-    return lastCell
+function getFreeCellOfColumn(column) {
+    cells = Array.from(column.getElementsByClassName(CELL_CLASS))
+    return cells.reverse().find(isCellFree)
+}
+
+function isCellFree(cell) {
+    return cell.getElementsByClassName(COIN_CLASS).length == 0
 }
 
 function startBoardWithWarn() {
-    if (confirm("Are you sure you want to restart your game?")) {
+    if (gameStarted) {
+        if (confirm("Are you sure you want to restart your game?")) {
+            startBoard()
+        }
+    }
+    else {
         startBoard()
     }
 }
 
-function handleMouseOver(event) {
-    const column = getColumnFromEventTarget(event.target)
+function onMouseOver(event) {
+    const column = getParentColumnOfNode(event.target)
     if (column !== undefined) {
-        column.style.background = turn;
+        column.classList.add(turn.class);
     }
 }
 
-function handleMouseLeave(event) {
-    const column = getColumnFromEventTarget(event.target)
+function onMouseLeave(event) {
+    const column = getParentColumnOfNode(event.target)
     if (column !== undefined) {
-        column.style.background = "white"
+        column.className = COLUMN_CLASS
     }
 }
 
 
-board.addEventListener("click", handleBoardClicked)
-board.addEventListener("mouseover", handleMouseOver)
-board.addEventListener("mouseout", handleMouseLeave)
+board.addEventListener("click", onBoardClicked)
+board.addEventListener("mouseover", onMouseOver)
+board.addEventListener("mouseout", onMouseLeave)
 
 document.getElementById("input-rows").addEventListener("change", startBoardWithWarn)
 document.getElementById("input-cols").addEventListener("change", startBoardWithWarn)
